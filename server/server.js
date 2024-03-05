4d0f78096afa8365c9ce519c2231412876938422
siriknikita <mykyta.siryk@lnu.edu.ua>
2024-03-06 00:25:11 +0200

    Praying for Google Authentication to Work

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ app.use(passport.session());
 
 app.use(
 	cors({
-		origin: "http://localhost:3000",
+		origin: "http://localhost:5050",
 		methods: "GET,POST,PUT,DELETE",
 		credentials: true,
 	})
