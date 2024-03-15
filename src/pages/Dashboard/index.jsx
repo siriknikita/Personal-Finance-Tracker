e0ddc8fe2e2b2ec34bab34190ff0477b6dc958e3
siriknikita <mykyta.siryk@lnu.edu.ua>
2024-03-15 18:35:16 +0200

    Updated Dashboard hrefs

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -16,7 +16,6 @@ function Dashboard(userDetails) {
             <h3>Discover</h3>
             <ul>
             <li>
-                <a href="#">
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width={24}
@@ -26,10 +25,8 @@ function Dashboard(userDetails) {
                     <path d="M20.205 4.791a5.938 5.938 0 0 0-4.209-1.754A5.906 5.906 0 0 0 12 4.595a5.904 5.904 0 0 0-3.996-1.558 5.942 5.942 0 0 0-4.213 1.758c-2.353 2.363-2.352 6.059.002 8.412l7.332 7.332c.17.299.498.492.875.492a.99.99 0 0 0 .792-.409l7.415-7.415c2.354-2.353 2.355-6.049-.002-8.416zm-1.412 7.002L12 18.586l-6.793-6.793c-1.562-1.563-1.561-4.017-.002-5.584.76-.756 1.754-1.172 2.799-1.172s2.035.416 2.789 1.17l.5.5a.999.999 0 0 0 1.414 0l.5-.5c1.512-1.509 4.074-1.505 5.584-.002 1.563 1.571 1.564 4.025.002 5.588z" />
                 </svg>
                 Most popular
-                </a>
             </li>
             <li>
-                <a href="#">
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width={24}
@@ -40,7 +37,6 @@ function Dashboard(userDetails) {
                     <circle cx="7.507" cy="7.505" r="1.505" />
                 </svg>
                 Categories
-                </a>
             </li>
             </ul>
         </section>
@@ -48,7 +44,6 @@ function Dashboard(userDetails) {
             <h3>Tools</h3>
             <ul>
             <li>
-                <a href="#">
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width={24}
@@ -58,10 +53,8 @@ function Dashboard(userDetails) {
                     <path d="M10 18a7.952 7.952 0 0 0 4.897-1.688l4.396 4.396 1.414-1.414-4.396-4.396A7.952 7.952 0 0 0 18 10c0-4.411-3.589-8-8-8s-8 3.589-8 8 3.589 8 8 8zm0-14c3.309 0 6 2.691 6 6s-2.691 6-6 6-6-2.691-6-6 2.691-6 6-6z" />
                 </svg>
                 Search
-                </a>
             </li>
             <li>
-                <a href="#">
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width={24}
@@ -71,7 +64,6 @@ function Dashboard(userDetails) {
                     <path d="M21 4H3a1 1 0 0 0-1 1v14a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V5a1 1 0 0 0-1-1zm-1 14H4V9.227l7.335 6.521a1.003 1.003 0 0 0 1.33-.001L20 9.227V18zm0-11.448l-8 7.11-8-7.111V6h16v.552z" />
                 </svg>
                 Messages
-                </a>
             </li>
             </ul>
         </section>
